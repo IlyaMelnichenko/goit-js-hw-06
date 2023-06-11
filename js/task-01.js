8c6bf27ffdf00302f9966fdff2e57a53f6a0c2c5
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-06-11 16:09:05 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesEl = document.querySelector('.categories');
+const categoriesItemElement = document.querySelectorAll('.item');
+
+console.log('Number of categories:',categoriesItemElement.length);
+categoriesItemElement.forEach(function (item,index){
+console.log('Category:',item.firstElementChild.textContent); 
+const qwe = item.querySelectorAll('li');
+console.log('Elements:',qwe.length)
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsUl = document.querySelector('.ingredients');
+ingredients.forEach(function(name,index){
+  
+const ingredientItem = document.createElement('li');
+ingredientItem.textContent=ingredients[index];
+ingredientItem.classList='item';
+console.log(ingredientItem);
+return ingredientItem;
+});
+console.log(ingredientItem);
