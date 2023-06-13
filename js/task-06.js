506f96ed8cec802bb77516a00a09b3ea03ebc810
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-06-13 16:57:36 +0300

    dgfh

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,9 @@ inputEl.addEventListener("blur",()=>{
     console.log(inputEl.value.length)
 if(inputEl.value.length === parseInt(inputEl.getAttribute('data-length'))){
     inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid')
 }else {
     inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid')
 }
 })
